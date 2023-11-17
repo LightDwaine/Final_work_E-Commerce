1d50b946b40359740825834e2c3ad6e3a541e145
Jonathan Oliveira <zorarugner@gmail.com>
2023-11-17 14:15:46 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # final_work_e-commerce
 O trabalho final da cadeira de Programação Web 1, que consiste em:
  - Desenvolver um e-commerce para permitir a comercialização de produtos online.
+Desenvolvedores:
+ _Pedro Henrique Nascimento de Lima_, - 508892,
+ _Guilherme Cavalcante Pires de Castro Studart_ - 509281,
+ _Jonathan Oliveira Silva_ - 509013,
+ _Edvando Ferreira Guilherme_ - 484852.
+
+Tecnologias Utilizadas: <br>
+Html - Linguagem de marcação <br>
+CSS - Linguagem de estilização <br>
+Java - Backend <br>
+JavaScript - Validação de formulário login/cadastro <br>
+PostgreSQL - Linguagem de Banco de Dados.
---
+++ src/java/produto/modelo/Produto.java
@@ -0,0 +1,66 @@
+package produto.modelo;
+
+/**
+ *
+ * @author Leonardo Oliveira Moreira
+ * 
+ * Classe que representa a entidade produto
+ */
+public class Produto {
+    
+    private int id;
+    private String descricao;
+    private double preco;
+    private String foto;
+    private int quantidade;
+    private int categoriaId;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(int categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+    
+}
